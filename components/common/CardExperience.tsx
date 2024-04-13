92843809546ddf2c325e5843275217f5afa21411
Marco Fura <mfura@kataricorp.com>
2024-04-13 00:23:44 -0400

    feat: add new experience

--- components/common/CardExperience.tsx
+++ components/common/CardExperience.tsx
@@ -7,19 +7,21 @@ export const CardExperience = ({
   subtitle
 }: ExperienceItem) => {
   return (
-    <div className="flex flex-col bg-green-600 py-2 px-4 border rounded-xl cursor-pointer ">
-      <div className="flex flex-col gap-2">
+    <div className="flex flex-col shadow-lg bg-gray-100 dark:bg-gray-200 dark:text-gray-800 py-2 px-4 border rounded-xl text-gray-600 w-[30rem]">
+      <div className="flex flex-col items-center gap-2">
         <h3 className="font-bold text-xl">{title}</h3>
         <p className="font-light text-md">{subtitle}</p>
         <p className="font-extralight text-sm">{rangeDate}</p>
       </div>
-      <div className="hidden">
+      <div className="text-md py-4">
         {typeof description === 'string' ? (
-          <p>{description}</p>
+          <p className="text-center">{description}</p>
         ) : (
           <ul>
             {description.map((item, index) => (
-              <li key={item + index}>{item}</li>
+              <li className="text-center" key={item + index}>
+                {item}
+              </li>
             ))}
           </ul>
         )}
